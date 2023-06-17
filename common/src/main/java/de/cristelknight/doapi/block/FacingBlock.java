7d1887e4a20d315f7c0ef744c584683ca610ddc4
Cristelknight999 <54945257+Cristelknight999@users.noreply.github.com>
2023-06-17 09:44:32 +0200

    maven pls

--- common/src/main/java/de/cristelknight/doapi/block/FacingBlock.java
+++ common/src/main/java/de/cristelknight/doapi/block/FacingBlock.java
@@ -12,7 +12,7 @@ public class FacingBlock extends HorizontalDirectionalBlock {
 
 	public FacingBlock(Properties settings) {
 		super(settings);
-		this.registerDefaultState(this.defaultBlockState().setValue(FACING, Direction.NORTH));
+		//this.registerDefaultState(this.defaultBlockState().setValue(FACING, Direction.NORTH));
 	}
 
 	@Nullable
